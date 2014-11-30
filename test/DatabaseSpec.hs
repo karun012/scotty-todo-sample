1bc6dcf3e4aa41ecaebfb68fc38afaa37b3fe6e5
Karun <karun012@gmail.com>
2014-11-29 18:44:10 -0800

    Phew. Finally figured out how to test something with AppState

--- test/DatabaseSpec.hs
+++ test/DatabaseSpec.hs
@@ -3,12 +3,28 @@ module DatabaseSpec where
 import Test.Hspec
 import Database
 
+import Data.Default
+import Control.Concurrent.STM
+import Control.Monad.Reader
+import qualified Data.Map as M
+
+import System.IO.Silently
+
 main :: IO ()
 main = hspec spec
 
 spec = do
     describe "Database" $ do
-        it "lets you add a todo item JSON, and returns an error if the string cannot be parsed into a Todo" $ do
-            addTodo "{\"do stuff\"}" `shouldBe` Left "Cannot parse JSON to Todo"
-        it "lets you add a todo item JSON, and returns a Todo if successful" $ do
-            addTodo "{\"text\" : \"do stuff\" }" `shouldBe` (Right $ Todo { uid = Nothing, text = Just "do stuff" })
+        it "returns a Failure with an error message if the json cannot be parsed to a Todo" $ do
+            let (result, w) = addTodoItem "{\"do stuff\"}"
+            result `shouldBe` (Failure "Cannot parse JSON to Todo")
+        it "adds new todos to AppState" $ do
+            sync <- newTVarIO def
+            let runM m = runReaderT (runWebM m) sync
+                (result, w) = addTodoItem "{\"text\" : \"do stuff\" }"
+            runM w
+            nextTodoId <- runM $ gets nextId
+            currentTodos <- runM $ gets todos
+            nextTodoId `shouldBe` 1
+            currentTodos `shouldBe` M.fromList [(1, Todo { text = Just "do stuff", uid = Just 1})]
+
